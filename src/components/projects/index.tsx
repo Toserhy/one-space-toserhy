d742d79aa30fd0ac2322e48ca2f06db440cd62f1
Toserhy <52323853+Toserhy@users.noreply.github.com>
2024-09-27 23:32:20 +0800

    Update index.tsx

--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -6,22 +6,6 @@ import Image from "next/image"
 
 const projectList = [
   {
-    img: "/projects/paint-board.png",
-    title: "Paint Board",
-    link: "https://songlh.top/paint-board",
-    github: "https://github.com/LHRUN/paint-board",
-    desc: "Paint Board is a powerful WEB-based creative drawing board that integrates a variety of creative brushes and assistive drawing functions, allowing users to experience a new drawing effect. All the content is free, no login, instant use. At present, it has supported multiple terminals, whether it is mobile terminal, or PC terminal, all have better interactive experience and effect display.",
-    author: "Paint Board is the project that I have invested the most effort in outside of work. From the preliminary research, to the prototype design and technical solution, to the in-depth study of colour matching and user interaction in the design stage, and the operation and promotion in the later stage, this project has greatly improved my ability in all aspects. Since its launch, Paint Board has also gained a lot of attention, which gives me a great sense of achievement and confidence to carry on. \n You are more than welcome to try it out! If you have any ideas, or found any bugs, please feel free to contact me, we can discuss and improve it together."
-  },
-  {
-    img: "/projects/bubble.png",
-    title: "Bubble",
-    link: "https://bubble-awesome-profile.vercel.app",
-    github: "https://github.com/LHRUN/bubble",
-    desc: "Bubble is a platform dedicated to collecting GitHub Profile components, bringing together the coolest Profile and README components on GitHub. Users can log in, like and mark their favourite profiles. Currently, there are various styles of Profile components on the platform, which can satisfy the needs of different developers. If you don't have any inspiration at the moment, you can also check out other people's excellent configurations and presentations in the Profile tab for inspiration.",
-    author: "Bubble was the first project I worked on when I first got into full-stack, using a technology stack of Next.js + Prisma + PostgreSQL + NextAuth, in the hope that it would open up the world of full-stack as a first step into independent development."
-  },
-  {
     img: "/projects/one-space.png",
     title: "One Space",
     link: "https://about.songlh.top",
@@ -29,16 +13,9 @@ const projectList = [
     desc: "One Space is the current website, as the first window to present myself, welcome to browse!"
   },
   {
-    img: "/projects/md-editor.png",
-    title: "MD Editor",
-    link: "https://songlh.top/md-editor",
-    github: "https://github.com/LHRUN/md-editor",
-    desc: "md-editor is a markdown-it based markdown editor, including sync scrolling, multi file record,upload and download, generate toc, menu edit btn, code block theme switch, content state local cache..."
-  },
-  {
     img: "/projects/work.svg",
     title: "My Work",
-    desc: "The above are open source projects that I develop in my spare time. At work, I am a front-end development engineer mainly responsible for C-end projects, focusing on media and e-commerce industry. My work involves multiple platforms, including PC, mobile, mini program and APP. Brands worked and served include Converse, Coach, UA, and DJCars.",
+    desc: "说实话我也不知自己在干什么，生活就像一个粪坑，工作就是里面一坨闪着金光的屎",
     maxWidth: "170px"
   }
 ]
