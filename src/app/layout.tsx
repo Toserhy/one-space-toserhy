5d045c6140490332a5a6698d21eaec4bf891a008
Toserhy <52323853+Toserhy@users.noreply.github.com>
2024-09-27 22:53:38 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
-  title: "One Space",
-  description: "One Space",
+  title: "Toserhy",
+  description: "personal page",
   icons: {
     icon: "/favicon.svg"
   }
